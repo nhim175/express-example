0bdc1dd5d2c51c5afcd58261a1a3bbdf140fd1ef
Thinh Pham <nhim175@gmail.com>
2018-09-21 22:39:15 +0900

    Create user validation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -35,6 +35,24 @@ module.exports.get = function(req, res) {
 
 module.exports.postCreate = function(req, res) {
   req.body.id = shortid.generate();
+  var errors = [];
+
+  if (!req.body.name) {
+    errors.push('Name is required.');
+  }
+
+  if (!req.body.phone) {
+    errors.push('Phone is required');
+  }
+
+  if (errors.length) {
+    res.render('users/create', {
+      errors: errors,
+      values: req.body
+    });
+    return;
+  }
+
   db.get('users').push(req.body).write();
   res.redirect('/users');
 };
